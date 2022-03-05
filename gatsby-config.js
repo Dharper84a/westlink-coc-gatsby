301fcf5a9d98d9c7ac4a5cd225e0001b5debf20f
Relix <Dharper84a@gmail.com>
2022-03-05 10:45:59 -0600

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
--- src/templates/Header/index.js
+++ src/templates/Header/index.js
@@ -1,8 +1,70 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
-const Header = () => {
+// Styles
+import * as styles from './styles.module.scss'
 
-  return <header>site header</header>
+const Header = (props) => {
+
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.siteHeader}>
+        <div className={styles.title}>
+          <StaticImage src="../../images/logo-white-bg.png" alt="Westlink Church of Christ logo" />
+          <span>Westlink Church of Christ</span>
+        </div>
+        
+        <nav className={styles.navigation}>
+          <ul>
+            <li>
+              About
+              <ul>
+                <li>
+                  <a href="/about/our-beliefs">Our Beliefs</a>
+                </li>
+                <li>
+                  <a href="/about/ministers-and-staff">Ministers and Staff</a>
+                </li>
+                <li>
+                  <a href="/about/missions">Missions</a>
+                </li>
+                
+              </ul>
+            </li>
+            <li>
+              Connect
+              <ul>
+                <li>
+                  <a href="/connect/visit-westlink">Visit Westlink</a>
+                </li>
+                <li>
+                  <a href="/connect/becoming-a-member">Becoming a Member</a>
+                </li>
+                <li>
+                  <a href="/connect/groups-and-ministries">Groups and Ministries</a>
+                </li>
+
+              </ul>
+            </li>
+            <li>
+              Media
+              <ul>
+                <li>
+                  <a href="/media/watch-live">Watch Live</a>
+                </li>
+                <li>
+                  <a href="/media/podcasts">Podcasts</a>
+                </li>
+                <li>
+                  <a href="/media/past-sermons">Past Sermons</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
 }
 
 export default Header
