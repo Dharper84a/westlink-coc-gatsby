cb835321852aa432bd34d4ef940814bf7ab2b918
Relix <Dharper84a@gmail.com>
2022-05-08 09:52:30 -0500

    fixed vidoe pages

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -3,6 +3,32 @@ import React, {useState, useRef, useEffect} from "react"
 import {useElementDimensions} from '../../hooks/useWindowDimensions';
 
 import * as styles from './VideoCard.module.scss';
+function escapeHtml(text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+  
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+function decodeHtml(str)
+{
+    var map =
+    {
+        '&amp;': '&',
+        '&lt;': '<',
+        '&gt;': '>',
+        '&quot;': '"',
+        '&#039;': "'",
+        '&#39;': "'",
+    };
+    return str.replace(/&amp;|&lt;|&gt;|&quot;|&#039;|&#39;/g, function(m) {return map[m];});
+}
+
 const VideoCard = props => {
   const videoContainerRef = useRef();
   const containerDimensions = useElementDimensions(videoContainerRef);
@@ -11,8 +37,8 @@ const VideoCard = props => {
   const videoSrc = `https://youtube.com/embed/${props.videoId}`
  
   const parser = new DOMParser();
-  const title = parser.parseFromString(props.title, 'text/html');
-
+  // const title = parser.parseFromString(props.title, 'text/html');
+  const title = decodeHtml(props.title);
   useEffect(() => {
     let value = containerDimensions.width * 0.56;
     setAspectHeight(value);
